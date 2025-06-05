8dd22642fae09011f6f00a8d21e837f89d4e0bea
Sahil Sinnarkar <sahilsinnarkar@gmail.com>
2025-06-06 00:58:47 +0530

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-10">
-      <SignInButton>
-        <Button>Sign in</Button>
-      </SignInButton>
+      Home page
     </div>
   );
 }
