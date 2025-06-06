d148f2e1987bb82484ab02ae896416f1be010882
Sahil Sinnarkar <sahilsinnarkar@gmail.com>
2025-06-07 00:20:59 +0530

    code editor

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,11 +1,123 @@
-import React from 'react'
+import {
+  CallControls,
+  CallingState,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+import { LayoutListIcon, LoaderIcon, UsersIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { use, useState } from "react";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./ui/resizable";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import EndCallButton from "./EndCallButton";
+import CodeEditor from "./CodeEditor";
 
 const MeetingRoom = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default MeetingRoom
+  const [layout, setLayout] = useState<"grid" | "speaker">("speaker");
+  const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
+
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) {
+    return (
+      <div className="h-96 flex items-center justify-center">
+        <LoaderIcon className="size-6 animate-spin" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-[calc(100vh-4rem-1px)]">
+      <ResizablePanelGroup direction="horizontal">
+        {/* left side */}
+        <ResizablePanel
+          defaultSize={35}
+          minSize={25}
+          maxSize={100}
+          className="relative"
+        >
+          {/* video layout */}
+          <div className="absolute inset-0">
+            {layout === "grid" ? <PaginatedGridLayout /> : <SpeakerLayout />}
+
+            {/* PARTICIPANTS LIST OVERLAY */}
+            {showParticipants && (
+              <div className="absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                <CallParticipantsList
+                  onClose={() => setShowParticipants(false)}
+                />
+              </div>
+            )}
+
+            {/* VIDEO CONTROLS */}
+            <div className="absolute bottom-4 left-0 right-0">
+              <div className="flex flex-col items-center gap-4">
+                <div className="flex items-center gap-2 flex-wrap justify-center px-4">
+                  <CallControls onLeave={() => router.push("/")} />
+
+                  <div className="flex items-center gap-2">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          variant="outline"
+                          size="icon"
+                          className="size-10"
+                        >
+                          <LayoutListIcon className="size-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+
+                      <DropdownMenuContent>
+                        <DropdownMenuItem onClick={() => setLayout("grid")}>
+                          Grid View
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => setLayout("speaker")}>
+                          Speaker View
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="size-10"
+                      onClick={() => setShowParticipants(!showParticipants)}
+                    >
+                      <UsersIcon className="size-4" />
+                    </Button>
+
+                    <EndCallButton />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ResizablePanel>
+
+        <ResizableHandle withHandle />
+
+        {/* right side */}
+        <ResizablePanel>
+          <CodeEditor />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
+
+export default MeetingRoom;
